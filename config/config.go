24cd996ef66fc13b2ecaaca87b00f059c7d6610d
Jinho <59327026+zzzinho@users.noreply.github.com>
2021-03-30 17:24:12 +0900

    Delete config directory

--- config/config.go
+++
@@ -1,9 +0,0 @@
-package config
-
-const (
-	MESSAGE_API_URL               = "https://talkapi.lgcns.com"
-	MESSAGE_API_KEY               = "kmEOONuQejAC9qcb8EYK0Q=="
-	MESSAGE_API_ID                = "groomer"
-	MESSAGE_NOTICE_SERVICE_NUMBER = 2110054713
-	FRIEND_SERVICE_NUMBER         = 2120054714
-)
