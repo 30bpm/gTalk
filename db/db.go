c825062c468a56e03680c6117a9c99f9c08ae64a
jinho <hahoh0013@gmail.com>
2021-04-05 17:48:24 +0900

    feat :: db

--- db/db.go
+++ db/db.go
@@ -3,6 +3,7 @@ package db
 import (
 	"fmt"
 
+	"github.com/groomer/gTalk/config"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -10,7 +11,7 @@ import (
 var DB *gorm.DB
 
 func Connect() {
-
+	dsn := fmt.Sprintf("%s:%s@tcp(%s)/groomer_talk?charset=utf8mb4&parseTime=True&loc=Local", config.DB_USER, config.DB_PASSWORD, config.DB_HOST)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		fmt.Println(err)
